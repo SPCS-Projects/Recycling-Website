61c5963b26078e590b1a6ac58fbbf1eac2e569c4
SPCS-Projects <73302976+SPCS-Projects@users.noreply.github.com>
2024-02-20 19:34:14 -0500

    fixed a minor error in redirect.js

--- redirect.js
+++ redirect.js
@@ -10,28 +10,27 @@ if (currentUser.loggedIn) {
     const query = wixData.query("users").eq('userID', currentUser.id);
     query.find()
         .then(results => {
-        //if user doesn't exist create an entry in the database
-        if (results.items.length === 0) {
-            wixData.insert("users", {"userID": currentUser.id, "points": 0})
-        }
-    currentUser.getRoles()
-        .then((roles) => {
-            let iter = roles["length"]
-            let userRoles = []
-            for (let i = 0; i < iter; i++) {
-                userRoles.push(roles[i]["name"])
+            if (results.items.length === 0) {
+                wixData.insert("users", { "userID": currentUser.id, "points": 0 })
+            }
+            currentUser.getRoles()
+                .then((roles) => {
+                    let iter = roles["length"]
+                    let userRoles = []
+                    for (let i = 0; i < iter; i++) {
+                        userRoles.push(roles[i]["name"])
 
-            }
-            //check if the user is an admin, if so take them to the admin page otherwise the main page that allows recycling
-            if (userRoles.includes("Admin")) {
-                wixLocation.to('/admin-dashboard');
-            } else {
-                wixLocation.to('/main');
-            }
+                    }
+                    if (userRoles.includes("Admin")) {
+                        wixLocation.to('/admin-dashboard');
+                    } else {
+                        wixLocation.to('/main');
+                    }
+
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
 
         })
-        .catch((error) => {
-            console.error(error);
-        });
-
 }
